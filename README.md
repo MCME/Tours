f2a6ba26afde218e0b94f873f7340bda6c8cd125
daggs <melhuishrob@gmail.com>
2014-05-21 07:46:36 +0100

    0_____o

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 Tours
-=====
+==========
+
+Tour management plugin.<br/>
+<i>Allows a 'guide' to create a temporary group that other players can join.<br/>
+Guides then have access to teleportation commands that can only be used on members of that group.<br/></i>
+<br/>
+<b>Automated Builds</b><br/>
+http://ci.dags.me/job/Tours/<br/>
+
+==========
+<b>Permissions</b><br/>
+<br/>
+All users with '<b>Tours.user</b>' permission node can:
+- '<b>/tour</b>' to find info on tours.
+- '<b>/tour help</b>' to find info on Tours commands.
+- '<b>/tour join \<tourname\></b>' to join a currently running tour.
+- '<b>/tour leave</b>' to leave the currently running tour.
+- '<b>/tour request</b>' sends an alert to any Rangers online that use is looking for a tour.
+- '<b>/ttp</b>' teleports the user to their tour guide.
+
+Users with the '<b>Tours.ranger</b>' permission node can:
+- '<b>/tour start</b>' creates new tour, names it after user, and announces it.
+- '<b>/tour stop</b>' stops user's current tour and removes all group members.
+- '<b>/tour list</b>' lists all users in the tour group.
+- '<b>/tour hat</b>' allows the tour guide to wear their currently held block as a hat.
+- '<b>/tourtp \<player\></b>' teleports \<player\> to user. \<player\> must be part of tour group.
+- '<b>/ttp \<player\></b>' shortcut for tourtp.
+- '<b>/tourtpa</b>' teleports all players in tour group to user.
+- '<b>/ttpa</b>' shortcut for tourtpa.<br/>
+
+==========
+
+<b>Contributions</b> <br/>
+Any contributions should - as best as possible - maintain the existing design paterns, conventions, and styles currently in place in this project.<br/>
+Deprecated or NMS code is to be avoided and subject to scrutiny.<br/>
+
+==========
+
+<b>License</b><br/>
+Tours is licensed under the GNU General Public License V3:<br/>
+http://www.gnu.org/copyleft/gpl.html<br/>
+<br/>
+<i>In the spirit of good-faith that this source has been made available, the author requests accreditation for works, in the form of the follow tag, to be appended to any class that contains his/her original code:<br/>
+(This request is an optional, additional courtesy)</i><br/>
+/**<br/>
+ \* @author dags_ <dags@dags.me><br/>
+ \*/<br/>
---
+++ src/main/java/com/mcmiddleearth/tours/listeners/PlayerListener.java
@@ -0,0 +1,122 @@
+package com.mcmiddleearth.tours.listeners;
+
+import com.mcmiddleearth.tours.tour.Tour;
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import static com.mcmiddleearth.tours.Tours.tourPlayers;
+import static com.mcmiddleearth.tours.Tours.tours;
+import static com.mcmiddleearth.tours.utils.Colors.*;
+/**
+ *
+ * @author dags_ <dags@dags.me>
+ */
+public class PlayerListener implements Listener
+{
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void playerJoin(PlayerJoinEvent e)
+    {
+        Player p = e.getPlayer();
+
+        if (!(p.hasPlayedBefore()))
+        {
+            for (Player q : Bukkit.getOnlinePlayers())
+            {
+                if (q.hasPermission("Tours.ranger"))
+                {
+                    q.sendMessage(lPurple + p.getName() + dPurple + " is a newbie to the server, give 'em a hug!");
+                }
+            }
+        }
+
+        if (tours.size() == 0)
+        {
+            p.sendMessage(yellow + "There are no tours running right now.");
+            p.sendMessage(yellow + "See " + green + "/tour help" + yellow + " for more info!");
+        }
+        if (tours.size() > 0)
+        {
+            p.sendMessage(green + "There are 1 or more tours currently running!");
+            p.sendMessage(yellow + "See " + green + "/tour help" + yellow + " for more info!");
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void playerQuit(PlayerQuitEvent e)
+    {
+        Player p = e.getPlayer();
+        if (tourPlayers.containsKey(p.getName()))
+        {
+            if (tours.containsKey(p.getName()))
+            {
+                leaderQuit(p);
+            }
+            else
+            {
+                touristQuit(p);
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void playerKicked(PlayerKickEvent e)
+    {
+        Player p = e.getPlayer();
+        if (tourPlayers.containsKey(p.getName()))
+        {
+            if (tours.containsKey(p.getName()))
+            {
+                leaderQuit(p);
+            }
+            else
+            {
+                touristQuit(p);
+            }
+        }
+    }
+
+    private void leaderQuit(Player p)
+    {
+        Tour t = tours.get(p.getName());
+        if (t == null)
+        {
+            return;
+        }
+        t.tourNotify(gray + "Your guide has left, the tour has ended!");
+
+        for (String s : t.getTouristList())
+        {
+            if (tourPlayers.containsKey(s))
+            {
+                tourPlayers.remove(s);
+            }
+        }
+
+        t.tourClear();
+        if (p.getInventory().getHelmet() != null)
+        {
+            if (p.getInventory() instanceof Block)
+                System.out.print("yes");
+            p.getInventory().setHelmet(null);
+        }
+    }
+
+    private void touristQuit(Player p)
+    {
+        Tour t = tours.get(tourPlayers.get(p.getName()));
+        if (t == null)
+        {
+            return;
+        }
+        t.removeTourist(p);
+        tourPlayers.remove(p.getName());
+    }
+}
